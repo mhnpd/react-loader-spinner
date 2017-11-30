6537d292250aaa565c1b24f7a02d4d151ff6a92b
Mohan Pd <mhnpd@outlook.com>
2017-11-30 23:33:50 +0545

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {audio as Audio} from './svgCollection/audio';
-import {ballTrangle as BallTrangle } from './svgCollection/ball-triangle';
-import {bars as Bars} from './svgCollection/bars';
-import {circles as Circles} from './svgCollection/circles';
-import {grid as Grid } from './svgCollection/grid';
-import {hearts as Hearts} from './svgCollection/hearts';
-import {oval as Oval } from './svgCollection/oval';
-import {puff as Puff} from './svgCollection/puff';
-import {rings as Rings} from './svgCollection/rings';
-import {tailSpin as TailSpin} from './svgCollection/tail-spin';
-import {threeDots as ThreeDots } from './svgCollection/three-dots';
+import {audio as Audio} from './loading/audio';
+import {ballTrangle as BallTrangle } from './loading/ball-triangle';
+import {bars as Bars} from './loading/bars';
+import {circles as Circles} from './loading/circles';
+import {grid as Grid } from './loading/grid';
+import {hearts as Hearts} from './loading/hearts';
+import {oval as Oval } from './loading/oval';
+import {puff as Puff} from './loading/puff';
+import {rings as Rings} from './loading/rings';
+import {tailSpin as TailSpin} from './loading/tail-spin';
+import {threeDots as ThreeDots } from './loading/three-dots';
 export default class Loader extends React.Component {
   static propTypes = {
       color: PropTypes.string,
