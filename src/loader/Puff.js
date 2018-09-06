3192aee4a1ecc484546500b47c85ebd79a057fcf
mohan <mohan@jyaasa.com>
2018-09-06 21:06:33 +0545

    Change function to const

---
+++ src/loader/Puff.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Puff = svg => (
+  <svg
+    width={svg.width}
+    height={svg.height}
+    viewBox="0 0 44 44"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke={svg.color}
+  >
+    <g fill="none" fillRule="evenodd" strokeWidth="2">
+      <circle cx="22" cy="22" r="1">
+        <animate
+          attributeName="r"
+          begin="0s"
+          dur="1.8s"
+          values="1; 20"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.165, 0.84, 0.44, 1"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="strokeOpacity"
+          begin="0s"
+          dur="1.8s"
+          values="1; 0"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.3, 0.61, 0.355, 1"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="22" cy="22" r="1">
+        <animate
+          attributeName="r"
+          begin="-0.9s"
+          dur="1.8s"
+          values="1; 20"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.165, 0.84, 0.44, 1"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="strokeOpacity"
+          begin="-0.9s"
+          dur="1.8s"
+          values="1; 0"
+          calcMode="spline"
+          keyTimes="0; 1"
+          keySplines="0.3, 0.61, 0.355, 1"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </g>
+  </svg>
+);
--- stories/index.js
+++ stories/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import  Loader  from '../src/index.js'
-storiesOf('Loader', module)
-    .add('Audio', () => <Loader width={200} height={200} type="Audio" visible="false"/>)
-    .add('Ball Triangle', () => <Loader width={200} heigth={300} color={'#cef442'} type="Ball-Triangle"/>)
-    .add('Bars', () => <Loader width={200} heigth={200}  color="green" type="Bars"/>)
-    .add('Circles', () => <Loader color="green" width={200} heigth={200}  type="Circles"/>)
-    .add('Grid', () => <Loader color="green" width={200} heigth={200}  type="Grid"/>) 
-    .add('Hearts', () => <Loader color="green" width={200} heigth={200}  type="Hearts"/>) 
-    .add('Oval', () => <Loader color="green" width={200} heigth={200}  type="Oval"/>) 
-    .add('Puff', () => <Loader color="green" width={200} heigth={200}  type="Puff"/>) 
-    .add('Rings', () => <Loader color="green"width={200} heigth={200}  type="Rings"/>) 
-    .add('TailSpin', () =>(<div> <Loader color="#00BFFF" width={200} heigth={200}  type="TailSpin"/> <Loader type="TailSpin" color="#939248" height="100" width="100" /> </div>)) 
-    .add('ThreeDots', () => <Loader color="green" width={200} heigth={200}  type="ThreeDots"/>); 
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
+import Loader from "../src/index";
 
+storiesOf("Loader", module)
+  .add("Audio", () => <Loader width={200} height={200} type="Audio" visible="false" />)
+  .add("Ball Triangle", () => (
+    <Loader width={200} heigth={300} color="#cef442" type="Ball-Triangle" />
+  ))
+  .add("Bars", () => <Loader width={200} heigth={200} color="green" type="Bars" />)
+  .add("Circles", () => <Loader color="green" width={200} heigth={200} type="Circles" />)
+  .add("Grid", () => <Loader color="green" width={200} heigth={200} type="Grid" />)
+  .add("Hearts", () => <Loader color="green" width={200} heigth={200} type="Hearts" />)
+  .add("Oval", () => <Loader color="green" width={200} heigth={200} type="Oval" />)
+  .add("Puff", () => <Loader color="green" width={200} heigth={200} type="Puff" />)
+  .add("Rings", () => <Loader color="green" width={200} heigth={200} type="Rings" />)
+  .add("TailSpin", () => (
+    <div>
+      {" "}
+      <Loader color="#00BFFF" width={200} heigth={200} type="TailSpin" />{" "}
+      <Loader type="TailSpin" color="#939248" height="100" width="100" />{" "}
+    </div>
+  ))
+  .add("ThreeDots", () => <Loader color="green" width={200} heigth={200} type="ThreeDots" />)
+  .add("NotSpecified", () => <Loader color="green" width={200} heigth={200} />);
