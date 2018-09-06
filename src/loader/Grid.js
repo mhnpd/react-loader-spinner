8db164b414e057894ca40be6c1b2ad0e31d9af84
mohan <mohan@jyaasa.com>
2018-09-06 21:06:33 +0545

    Change function to const

---
+++ src/loader/Grid.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+export const Grid = svg => (
+  <svg width={svg.width} height={svg.height} viewBox="0 0 105 105" fill={svg.color}>
+    <circle cx="12.5" cy="12.5" r="12.5">
+      <animate
+        attributeName="fillOpacity"
+        begin="0s"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="12.5" cy="52.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="100ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="52.5" cy="12.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="300ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="52.5" cy="52.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="600ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="92.5" cy="12.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="800ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="92.5" cy="52.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="400ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="12.5" cy="92.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="700ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="52.5" cy="92.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="500ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <circle cx="92.5" cy="92.5" r="12.5">
+      <animate
+        attributeName="fill-opacity"
+        begin="200ms"
+        dur="1s"
+        values="1;.2;1"
+        calcMode="linear"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
+);
