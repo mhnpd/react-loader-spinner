cf81baebfcef1126a60ada008ff0a185619cd032
Mohan Pd <mohan@jyaasa.com>
2019-09-09 16:42:12 +0545

    Update eslint version

--- dist/index.js
+++ dist/index.js
@@ -42,35 +42,6 @@
     return target;
   };
 
-  function _asyncToGenerator(fn) {
-    return function () {
-      var gen = fn.apply(this, arguments);
-      return new Promise(function (resolve, reject) {
-        function step(key, arg) {
-          try {
-            var info = gen[key](arg);
-            var value = info.value;
-          } catch (error) {
-            reject(error);
-            return;
-          }
-
-          if (info.done) {
-            resolve(value);
-          } else {
-            return Promise.resolve(value).then(function (value) {
-              step("next", value);
-            }, function (err) {
-              step("throw", err);
-            });
-          }
-        }
-
-        return step("next");
-      });
-    };
-  }
-
   var _slicedToArray = function () {
     function sliceIterator(arr, i) {
       var _arr = [];
@@ -122,8 +93,6 @@
    * @return {null}
    */
   function Loader(props) {
-    var _this = this;
-
     var _useState = (0, _react.useState)(true),
         _useState2 = _slicedToArray(_useState, 2),
         display = _useState2[0],
@@ -131,21 +100,9 @@
 
     (0, _react.useEffect)(function () {
       if (props.timeout && props.timeout > 0) {
-        setTimeout(_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-          return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-              switch (_context.prev = _context.next) {
-                case 0:
-                  _context.next = 2;
-                  return setDisplay(false);
-
-                case 2:
-                case "end":
-                  return _context.stop();
-              }
-            }
-          }, _callee, _this);
-        })), props.timeout);
+        setTimeout(function () {
+          setDisplay(false);
+        }, props.timeout);
       }
     });
 
