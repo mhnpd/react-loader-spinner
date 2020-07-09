41600b928be94827cc8582e2b5014fd202a0d8ee
JoostVanVelthoven <vanvelthoven@gmail.com>
2020-07-09 19:16:37 +0200

    mouseover effect to see the name of the spinner. (#62)
    Like this project very much, thank you for sharing.

--- example/src/App.js
+++ example/src/App.js
@@ -9,13 +9,13 @@ function App() {
         <h1 className=""> React Spinner</h1>
         <div className="container">
           <div className="row">
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Puff">
               <Loader type="Puff" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Audio">
               <Loader type="Audio" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5"  title="BallTriangle">
               <Loader
                 type="BallTriangle"
                 color="#00BFFF"
@@ -23,29 +23,29 @@ function App() {
                 width={100}
               />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Bars">
               <Loader type="Bars" color="#00BFFF" height={100} width={100} />
             </div>
           </div>
-          <div className="row">
+          <div className="row" title="Circles">
             <div className="col spinner_item p-5">
               <Loader type="Circles" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Grid">
               <Loader type="Grid" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Hearts">
               <Loader type="Hearts" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Oval">
               <Loader type="Oval" color="#00BFFF" height={100} width={100} />
             </div>
           </div>
-          <div className="row">
+          <div className="row" title="Rings">
             <div className="col spinner_item p-5">
               <Loader type="Rings" color="#00BFFF" height={100} width={100} />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="TailSpin">
               <Loader
                 type="TailSpin"
                 color="#00BFFF"
@@ -53,7 +53,7 @@ function App() {
                 width={100}
               />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="ThreeDots">
               <Loader
                 type="ThreeDots"
                 color="#00BFFF"
@@ -61,12 +61,12 @@ function App() {
                 width={100}
               />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Watch">
               <Loader type="Watch" color="#00BFFF" height={100} width={100} />
             </div>
           </div>
           <div className="row">
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="RevolvingDot">
               <Loader
                 type="RevolvingDot"
                 color="#00BFFF"
@@ -74,7 +74,7 @@ function App() {
                 width={100}
               />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="Triangle">
               <Loader
                 type="Triangle"
                 color="#00BFFF"
@@ -82,7 +82,7 @@ function App() {
                 width={100}
               />
             </div>
-            <div className="col spinner_item p-5">
+            <div className="col spinner_item p-5" title="MutatingDots">
               <Loader
                 type="MutatingDots"
                 color="#00BFFF"
