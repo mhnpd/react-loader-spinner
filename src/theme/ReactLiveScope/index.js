d3bcf86a21f8f04ef413ab27b154eb2a191068c3
Mohan <mhnpd@outlook.com>
2022-07-23 23:51:45 +0200

    Remove default files

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import { Audio } from 'react-loader-spinner';
+import React from 'react'
+import {
+  Audio,
+  BallTriangle,
+  Bars,
+  Circles,
+  CirclesWithBar,
+  Grid,
+  Hearts,
+  LineWave,
+  MutatingDots,
+  Oval,
+  Plane,
+  RevolvingDot,
+  Rings,
+  RotatingSquares,
+  TailSpin,
+  ThreeCircles,
+  ThreeDots,
+  Triangle,
+  Watch
+} from 'react-loader-spinner'
+
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
   Audio,
-};
-export default ReactLiveScope;
+  BallTriangle,
+  Bars,
+  Circles,
+  CirclesWithBar,
+  Grid,
+  Hearts,
+  LineWave,
+  MutatingDots,
+  Oval,
+  Plane,
+  RevolvingDot,
+  Rings,
+  RotatingSquares,
+  TailSpin,
+  ThreeCircles,
+  ThreeDots,
+  Triangle,
+  Watch
+}
+export default ReactLiveScope
