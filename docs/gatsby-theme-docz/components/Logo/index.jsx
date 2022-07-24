651ebaac6286d6a42960c0d623d01cb4c1afbdea
Mohan <mhnpd@outlook.com>
2022-07-24 09:23:11 +0200

    remove docs folder from main

--- docs/gatsby-theme-docz/components/Logo/index.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-use-before-define
-import React from 'react'
-import LogoImage from './logo.png'
-
-export const Logo = () => {
-  return (
-    <img src={LogoImage} style={{ width: '285px', paddingTop: '23px' }} alt="React Spinners" />
-  )
-}
--- doczrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-  src: './docs',
-  ignore: ['README.md', 'Contributing.md'],
-  typescript: true,
-  themesDir: './docs/',
-  pathPrefix: '/react-loader-spinner',
-  base: '/react-loader-spinner'
-}
