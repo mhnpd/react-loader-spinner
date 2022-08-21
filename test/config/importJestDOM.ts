307367e54e3062c31dce082ec1b0164f96a90d06
Mohan <mhnpd@outlook.com>
2022-08-21 12:10:26 +0200

    [Test]: Add a jest configuration
    Test will throw an error if there is any react warning on the component.

--- test/config/importJestDOM.ts
+++ test/config/importJestDOM.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom'
+
+const CONSOLE_FAIL_TYPES = ['error', 'warn']
+
+// Throw errors when a `console.error` or `console.warn` happens
+// by overriding the functions
+CONSOLE_FAIL_TYPES.forEach((type) => {
+  // @ts-ignore
+  // eslint-disable-next-line no-console
+  console[type] = (message) => {
+    throw new Error(
+      `Failing due to console.${type} while running test!\n\n${message}`,
+    )
+  }
+})
