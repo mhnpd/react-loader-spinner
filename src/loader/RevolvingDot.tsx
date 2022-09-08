906a27581729c5ec95b4d7bf0eea048a065b5703
Atishay Jain <66154210+Atishay-J@users.noreply.github.com>
2022-09-08 13:08:30 +0530

    Fix:revolving dot issue #140 (#141)
    * WIP: Fix initial canvas and ring overflow issue
    
    * Fix: Revolving dot scaling issue
    
    * Chore: Remove unused code
    
    * Test: change test cases
    
    * Refactor: Change 
    adius and strokeWidth type, also increase default loader size
    
    * Test: Update test for new default configuration

--- src/loader/RevolvingDot.tsx
+++ src/loader/RevolvingDot.tsx
@@ -3,15 +3,16 @@ import { getDefaultStyle } from '../helpers'
 import { BaseProps, DEFAULT_COLOR, DEFAULT_WAI_ARIA_ATTRIBUTE } from '../type'
 
 interface RevolvingDotProps extends BaseProps {
-  radius?: string | number
+  radius?: number
   secondaryColor?: string
+  strokeWidth?: number
 }
 
 const RevolvingDot: FunctionComponent<RevolvingDotProps> = ({
-  height = 100,
-  width = 100,
-  radius = 6,
+  radius = 45,
+  strokeWidth = 5,
   color = DEFAULT_COLOR,
+  secondaryColor,
   ariaLabel = 'revolving-dot-loading',
   wrapperStyle,
   wrapperClass,
@@ -26,8 +27,8 @@ const RevolvingDot: FunctionComponent<RevolvingDotProps> = ({
   >
     <svg
       version="1.1"
-      width={width}
-      height={height}
+      width={`calc(${radius} * 2.5)`}
+      height={`calc(${radius} * 2.5)`}
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
@@ -35,27 +36,28 @@ const RevolvingDot: FunctionComponent<RevolvingDotProps> = ({
     >
       <circle
         fill="none"
-        stroke={color}
-        strokeWidth="4"
-        cx="50"
-        cy="50"
-        r={Number(`${radius}`) + 38}
+        stroke={secondaryColor || color}
+        strokeWidth={strokeWidth}
+        cx={`calc(${radius} * 1.28)`}
+        cy={`calc(${radius} * 1.28)`}
+        r={radius}
         style={{ opacity: 0.5 }}
       />
       <circle
         fill={color}
         stroke={color}
         strokeWidth="3"
-        cx="8"
-        cy="54"
-        r={radius}
+        cx={`calc(${radius} * 1.28)`}
+        cy={`calc(${radius} / 3.5)`}
+        r={`calc(${radius} / 5)`}
+        style={{ transformOrigin: '50% 50%' }}
       >
         <animateTransform
           attributeName="transform"
           dur="2s"
           type="rotate"
-          from="0 50 48"
-          to="360 50 52"
+          from="0"
+          to="360"
           repeatCount="indefinite"
         />
       </circle>
