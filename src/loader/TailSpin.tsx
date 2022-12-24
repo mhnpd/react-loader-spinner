b9cf9877a792be11b3ec65931dde5af692a06dba
Samy Rahmani <samy.osmium@gmail.com>
2022-12-24 12:03:38 -0500

    Fixed parsing issues

--- src/loader/TailSpin.tsx
+++ src/loader/TailSpin.tsx
@@ -17,59 +17,65 @@ export const TailSpin: FunctionComponent<TailSpinProps> = ({
   wrapperStyle,
   wrapperClass,
   visible = true,
-}): ReactElement => (
-  <div
-    style={{ ...getDefaultStyle(visible), ...wrapperStyle }}
-    className={wrapperClass}
-    data-testid="tail-spin-loading"
-    aria-label={ariaLabel}
-    {...DEFAULT_WAI_ARIA_ATTRIBUTE}
-  >
-    <svg
-      width={width}
-      height={height}
-      viewBox={`0 0 ${strokeWidth as number + 36} ${strokeWidth as number + 36}`}
-      xmlns="http://www.w3.org/2000/svg"
-      data-testid="tail-spin-svg"
+}): ReactElement => {
+  const strokeWidthNum = parseInt(String(strokeWidth));
+  const viewBoxValue = strokeWidthNum + 36;
+  const halfStrokeWidth = strokeWidthNum / 2;
+  const processedRadius = halfStrokeWidth + parseInt(String(radius)) - 1;
+  return (
+    <div
+      style={{ ...getDefaultStyle(visible), ...wrapperStyle }}
+      className={wrapperClass}
+      data-testid="tail-spin-loading"
+      aria-label={ariaLabel}
+      {...DEFAULT_WAI_ARIA_ATTRIBUTE}
     >
-      <defs>
-        <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
-          <stop stopColor={color} stopOpacity="0" offset="0%" />
-          <stop stopColor={color} stopOpacity=".631" offset="63.146%" />
-          <stop stopColor={color} offset="100%" />
-        </linearGradient>
-      </defs>
-      <g fill="none" fillRule="evenodd">
-        <g transform={`translate(${strokeWidth as number / 2} ${strokeWidth as number / 2})`}>
-          <path
-            d="M36 18c0-9.94-8.06-18-18-18"
-            id="Oval-2"
-            stroke={color}
-            strokeWidth={strokeWidth}
-          >
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="0 18 18"
-              to="360 18 18"
-              dur="0.9s"
-              repeatCount="indefinite"
-            />
-          </path>
-          <circle fill="#fff" cx="36" cy="18" r={(strokeWidth as number / 2) + (radius as number - 1)}>
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="0 18 18"
-              to="360 18 18"
-              dur="0.9s"
-              repeatCount="indefinite"
-            />
-          </circle>
+      <svg
+        width={width}
+        height={height}
+        viewBox={`0 0 ${viewBoxValue} ${viewBoxValue}`}
+        xmlns="http://www.w3.org/2000/svg"
+        data-testid="tail-spin-svg"
+      >
+        <defs>
+          <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+            <stop stopColor={color} stopOpacity="0" offset="0%" />
+            <stop stopColor={color} stopOpacity=".631" offset="63.146%" />
+            <stop stopColor={color} offset="100%" />
+          </linearGradient>
+        </defs>
+        <g fill="none" fillRule="evenodd">
+          <g transform={`translate(${halfStrokeWidth} ${halfStrokeWidth})`}>
+            <path
+              d="M36 18c0-9.94-8.06-18-18-18"
+              id="Oval-2"
+              stroke={color}
+              strokeWidth={strokeWidth}
+            >
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite"
+              />
+            </path>
+            <circle fill="#fff" cx="36" cy="18" r={processedRadius}>
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite"
+              />
+            </circle>
+          </g>
         </g>
-      </g>
-    </svg>
-  </div>
-)
+      </svg>
+    </div>
+  )
+}
 
 export default TailSpin
