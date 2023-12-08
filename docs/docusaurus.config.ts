af666a309cf66c493da2bb31b6b333e5dda91ab0
Mohan Upadhyay <mhnpd@outlook.com>
2023-12-08 15:00:11 +0545

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'mhnpd/react-loader-spinner',
+  baseUrl: '/react-loader-spinner/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
