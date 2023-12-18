e805f58fa6c16d874895ae6db339a26309f6be5d
mhnpd <mhnpd@outlook.com>
2023-12-18 13:59:39 +0545

    Update Jest test environment to jest-environment-jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/test/config/importJestDOM.ts'],
   transform: {
     '\\.ts$': 'ts-jest',
